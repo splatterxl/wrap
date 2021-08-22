ff83eafd4e8fb7c62805348875e0059f31414122
Splatterxl <splatterxl@outlook.ie>
2021-08-22 14:45:32 +0100

    document crate & use custom size variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,57 @@
+/// The default limit for line size.
+///
+/// This can be changed via the [`limit`] parameter in [wrap()].
+///
+/// [wrap()]: fn.wrap.html
+/// [`limit`]: fn.wrap.html#custom-limit
 pub const LIMIT: usize = 80;
 
-pub fn wrap(text: String) -> String {
-    let lines = text.split(|c| c == '\n');
+/// Wrap a `String`'s lines when they go over the specified `limit` (default `80`).
+///
+/// # Usage
+///
+/// A common usage of this function would go something like this:
+///
+/// ```
+/// use wrap::wrap;
+/// use std::fs;
+///
+/// fn main() {
+///     let file = fs::read_to_string("file.txt").unwrap();
+///     // this is just for the example, don't do it in your actual code!
+///     // you could use .unwrap_or() instead.
+///     println!("{}", wrap(file, None));
+/// }
+///
+/// ```
+///
+/// That code isn't safe, though. There's a [folder full of examples][examples] in the [GitHub repository][repo] which you can explore (and maybe take some *inspiration* from)!
+///
+/// # Custom Limit
+///
+/// It is possible to set a custom line width limit with the `limit` argument. In the binary executable bundled with the crate, it queries the `WRAP_LINE_SIZE` environment variable, using code similar to this:
+///
+/// ```
+/// let limit = env!("WRAP_LINE_SIZE");
+///
+/// wrap(text, Some(limit))
+/// ```
+///
+/// Of course, you can provide hard-coded values as well (although it isn't recommended!), and any
+/// `usize` is accepted.
+///
+/// Ergo, `(usize) 30` is valid, as well as `(usize) 1)`! Although, why would you even try to set the limit to `1`?!
+///
+/// [examples]: https://github.com/nearlySplat/wrap/tree/trunk/examples
+/// [repo]: https://github.com/nearlySplat/wrap
+pub fn wrap(text: String, limit: Option<usize>) -> String {
+    let limit = limit.unwrap_or(LIMIT);
+    let lines = text.split('\n');
 
     let mut to_return = "".to_owned();
 
     for line in lines {
-        let words = line.split(|c| c == ' ');
+        let words = line.split(' ');
 
         let mut new_line = "".to_owned();
 
@@ -21,7 +66,7 @@ pub fn wrap(text: String) -> String {
 
             size += &len;
 
-            if size <= LIMIT {
+            if size <= limit {
                 let space = {
                     if new_line.is_empty() {
                         ""
@@ -50,8 +95,10 @@ mod tests {
 
     #[test]
     pub fn does_it_work() {
-        let gnu = fs::read_to_string("test/test_case.txt").expect("Did not find test_case.txt file");
-        let to_assert = fs::read_to_string("test/wrapped.txt").expect("Did not find wrapped.txt file");
-        assert_eq!(crate::wrap(gnu), to_assert.trim_end());
+        let gnu =
+            fs::read_to_string("test/test_case.txt").expect("Did not find test_case.txt file");
+        let to_assert =
+            fs::read_to_string("test/wrapped.txt").expect("Did not find wrapped.txt file");
+        assert_eq!(crate::wrap(gnu, None), to_assert.trim_end());
     }
 }
