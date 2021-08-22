f9a14b07328b79d870b1310c1f3633307bf52cec
Splatterxl <splatterxl@outlook.ie>
2021-08-22 14:06:07 +0100

    clean up tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,10 @@ pub fn wrap(text: String) -> String {
         let mut size: usize = 0;
 
         for word in words.clone() {
+            if word.is_empty() {
+                new_line = format!("{} ", new_line);
+                continue;
+            }
             let len = word.len();
 
             size += &len;
@@ -30,6 +34,7 @@ pub fn wrap(text: String) -> String {
             } else {
                 new_line = format!("{}\n{}", new_line, word);
                 size = len;
+                continue;
             }
         }
 
@@ -45,23 +50,8 @@ mod tests {
 
     #[test]
     pub fn does_it_work() {
-        let gnu = fs::read_to_string("gnu.txt").expect("Did not find gnu.txt file in CWD");
-        assert_eq!(crate::wrap(gnu), "What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it,\n\
-GNU plus Linux. Linux is not an operating system unto itself, but rather another free component\n\
-of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital\n\
-system components comprising a full OS as defined by POSIX.\n\
-\n\
-Many computer users run a modified version of the GNU system every day, without realizing it.\n\
-Through a peculiar turn of events, the version of GNU which is widely used today is often called\n\
-Linux, and many of its users are not aware that it is basically the GNU system, developed by the\n\
-GNU Project.\n\
-\n\
-There really is a Linux, and these people are using it, but it is just a part of the system they use.\n\
-Linux is the kernel: the program in the system that allocates the machine's resources to the\n\
-other programs that you run. The kernel is an essential part of an operating system, but useless\n\
-by itself; it can only function in the context of a complete operating system. Linux is normally\n\
-used in combination with the GNU operating system: the whole system is basically GNU with Linux\n\
-added, or GNU/Linux. All the so-called Linux distributions are really distributions of\n\
-GNU/Linux!")
+        let gnu = fs::read_to_string("test/test_case.txt").expect("Did not find test_case.txt file");
+        let to_assert = fs::read_to_string("test/wrapped.txt").expect("Did not find wrapped.txt file");
+        assert_eq!(crate::wrap(gnu), to_assert.trim_end());
     }
 }
